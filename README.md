aa8ef41dd23e5de4bb4fb94d3f4dac99b8ec7a37
Michał Myśków <72620908+MichalMyskow@users.noreply.github.com>
2022-11-29 08:59:09 +0100

    #2 - project setup (#3)
    * #2 - dockerized example React app
    
    * #2 - basic linter setup
    
    * #2 - basic workflow setup
    
    * #2 - fix readme
    
    * #2 - setup fixes

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# praktyki
-Strona dla programu praktyk
+# Praktyki
+### Strona dla programu praktyk
+
+### Copy `.env` file
+
+```
+cp .env.example .env
+```
+
+### Docker development
+
+```
+docker-compose up -d
+```
+
+### Installation
+
+```
+docker-compose exec node npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+docker-compose exec node npm run dev
+```
+
+### Compiles and minifies for production
+
+```
+docker-compose exec node npm run build
+```
+
+### Lints files
+
+```
+docker-compose exec node npm run lint
+```
+
+### Available containers
+
+| service | container name | default external port |
+| ------- |----------------|-----------------------|
+| node    | praktyki-node  | 3000                  |
---
+++ src/pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Praktyki</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
