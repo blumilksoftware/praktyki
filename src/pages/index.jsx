6defb4791c61cfe52893456ef7afac9e4429f0f0
EwelinaLasowy <ewelina.lasowy@blumilk.pl>
2022-12-08 13:47:56 +0100

    #4 - wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         <title>Praktyki w Blumilku</title>
         <meta
           name="description"
-          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+          content="Zdobądź cenne doświadczenie zawodowe jako praktykant w Blumilku."
         />
       </Head>
       <Header />
