460cb1f2dfbcfd0ac107094e981afbbc935dfcba
Michał Myśków <72620908+MichalMyskow@users.noreply.github.com>
2023-02-13 12:49:41 +0100

    #1 - fix deploy paths (#14)
    * #1 - fix paths
    
    * #1 - fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}`
+  basePath = `/${repo}`
+}
+
+/** @type {import('next').NextConfig} */
+const nextDeployConfig = {
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,4 +28,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = isGithubActions ? nextDeployConfig : nextConfig
