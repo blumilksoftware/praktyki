f330df6de3a4a9fcced82e66d547282ea93244b4
MichalMyskow <michal.myskow@blumilk.pl>
2023-02-13 12:28:08 +0100

    #1 - fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true
-  }
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
